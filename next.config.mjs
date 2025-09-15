25f1abb8fb41062f45a201a278ef34f7b7f10ff2
hyyyyjin <hyyyyjin@naver.com>
2025-09-15 23:28:29 +0900

    update base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -11,8 +13,8 @@ const nextConfig = {
   },
   // GitHub Pages 설정
   output: 'export',
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/maternity-leave-calculator' : '',
+  // trailingSlash: true, // 루트 도메인에서는 이 옵션을 제거하는 것을 권장합니다.
+  basePath: '', // 모든 환경에서 basePath를 빈 문자열로 고정합니다.
 }
 
-export default nextConfig
+export default nextConfig;
